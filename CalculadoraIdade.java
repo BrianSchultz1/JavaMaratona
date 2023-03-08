04945146702d7173f6120a90b1aa80c52c0b888f
Brian Schultz <68474136+BrianSchultz1@users.noreply.github.com>
2023-03-08 16:26:24 -0300

    Add files via upload

---
+++ CalculadoraIdade.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//Faça um programa em Java que receba a idade de uma pessoa
+//expressa em anos, meses e dias, calcule e informe quantos dias
+//a pessoa já viveu. Considere que todos os meses têm 30 dias.
+public class CalculadoraIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int anos, meses, dias, totalDias;
+        System.out.println("informe quantos anos você tem");
+        anos = scan.nextInt();
+
+        System.out.println("informe quantos anos você tem");
+        meses = scan.nextInt();
+
+        System.out.println("informe quantos anos você tem");
+        dias = scan.nextInt();
+
+        totalDias = anos * 365 * meses * 30 * dias;
+        System.out.printf("você já viveu: " + totalDias);
+    }
+}
